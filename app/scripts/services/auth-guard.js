1ec714d85a477be8219ba439cbd5c19687e232f9
mzvast <mzvast@gmail.com>
2016-10-16 14:18:44 +0800

    fix another display issues

--- app/scripts/services/auth-guard.js
+++ app/scripts/services/auth-guard.js
@@ -11,7 +11,7 @@ angular.module('manageApp')
   .service('authGuard', function () {
     // AngularJS will instantiate a singleton by calling "new" on this function
     var vm = this;
-    vm.isLoggedIn = false;
+    vm.isLoggedIn = true;//false;TODO 开发方便，自动登录
     vm.login = function () {
       console.log("logged in");
       vm.isLoggedIn = true;
