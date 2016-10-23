2f3df68091c7f803b4307e6e434db9379e0adcaf
mzvast <mzvast@gmail.com>
2016-10-23 17:22:05 +0800

    analyze完成demo

--- app/scripts/components/analyze/component.js
+++ app/scripts/components/analyze/component.js
@@ -17,7 +17,15 @@ function analyzeController(authGuard,$state,analyzeService) {
   var $ctrl = this;
 
   $ctrl.$onInit = function () {
-    $ctrl.labels = ["单元测试", "集成测试", "功能测试","性能测试"];
+    $ctrl.productLabels = ["单元测试", "集成测试", "功能测试","性能测试"];
+    $ctrl.productTabModel = [{
+      id:0,
+      name:'用例结构'//'饼图'
+    },{
+      id:1,
+      name:'通过率'//'柱状图'
+    }];
+    //所有产品
     analyzeService.get({category:'products'},function (response) {
       var data = response.data;
       var allArr = data.map(function (item) {
@@ -26,19 +34,56 @@ function analyzeController(authGuard,$state,analyzeService) {
       var passArr = data.map(function (item) {
         return item.pass;
       });
-      $ctrl.pieData = allArr;
-      $ctrl.barData = [allArr,passArr];
+      $ctrl.allProductsPieData = allArr;
+      $ctrl.allProductsBarData = [allArr,passArr];
 
-      $ctrl.title = '结果分类统计';
+      $ctrl.allProductsTitle = '产品|全部';
+    });
+    //特定产品
+    analyzeService.get({category:'products',id:1},function (response) {
+      var data = response.data;
+      var allArr = data.map(function (item) {
+        return item.all;
+      });
+      var passArr = data.map(function (item) {
+        return item.pass;
+      });
+      $ctrl.oneProductPieData = allArr;
+      $ctrl.oneProductBarData = [allArr,passArr];
+
+      $ctrl.oneProductTitle = '某产品-某测试计划';
+    });
+
+    $ctrl.vendorLabels = ["通过","失败"];
+
+    $ctrl.vendorTabModel = [{
+      id:0,
+      name:'通过率'//'饼图'
+    }];
+    //所有厂家
+    analyzeService.get({category:'vendors'},function (response) {
+      var data = response.data;
+      var all = data.all;
+      var pass = data.pass;
+      var fail = all - pass;
+
+      $ctrl.allVendorsPieData = [all,fail];
+      $ctrl.allVendorsBarData = [all,fail];
+
+      $ctrl.allVendorsTitle = '厂家|全部';
+    });
+    //特定厂家
+    analyzeService.get({category:'vendors',id:1},function (response) {
+      var data = response.data;
+      var all = data.all;
+      var pass = data.pass;
+      var fail = all - pass;
+
+      $ctrl.oneVendorPieData = [all,fail];
+      $ctrl.oneVendorBarData = [all,fail];
+
+      $ctrl.oneVendorTitle = '某厂家-某测试计划';
     });
 
   };
-
-  //TODO 所有厂家
-  //TODO 特定厂家
-  //TODO 所有产品
-  //TODO 特定产品
-
-
-
 }
--- app/scripts/components/shared/analyzeChart/component.js
+++ app/scripts/components/shared/analyzeChart/component.js
@@ -11,7 +11,8 @@ angular.module('manageApp')
       barData: '<',
       labels: '<',
       options: '<',
-      title: '<'
+      title: '<',
+      tabModel: '<'
     },
     controller: analyzeChartController
   });
@@ -22,15 +23,7 @@ function analyzeChartController() {
   var $ctrl = this;
 
   $ctrl.$onInit = function () {
-    $ctrl.activeTabId = $ctrl.defaultActiveTabId!==undefined?$ctrl.defaultActiveTabId:0;
-
-    $ctrl.tabModel = [{
-      id:0,
-      name:'用例结构'//'饼图'
-    },{
-      id:1,
-      name:'通过率'//'柱状图'
-    }];
+    $ctrl.activeTabId = $ctrl.defaultActiveTabId||0;
 
     $ctrl.series = ['总数','通过数']
   };
